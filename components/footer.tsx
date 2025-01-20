fd8ac49be34d797844bfe3832d1daf4f56d58ae8
ManzanoFernanda <manzanofernandadaniela@gmail.com>
2025-01-20 12:44:13 -0300

    footer2

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,8 +4,8 @@ import MainLayout from "./main-layout";
 const Footer = () => {
   const currentYear = new Date().getFullYear();
   return (
-    <footer className="w-full border-t-2 border-solid border-dark font-medium text-sm md:text-lg dark:text-light dark:border-light fixed bottom-0 left-0 bg-yellow">
-      <MainLayout className="py-4 px-4 md:py-2 flex items-center justify-center text-center">
+    <footer className="w-full border-t-1 border-solid border-dark font-medium text-sm md:text-lg dark:text-light dark:border-light fixed bottom-0 left-0 bg-yellow">
+      <MainLayout className="py-2 px-2 md:py-1 flex items-center justify-center text-center">
         <span style={{ fontFamily: 'Arial' }}>&copy; {currentYear} Daniela Manzano</span>
       </MainLayout>
     </footer>
